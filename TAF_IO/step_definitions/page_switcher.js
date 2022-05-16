145212444d4d11cdeef17875fdf5e292181f3aa4
ZhulehaMikita <99655783+ZhulehaMikita@users.noreply.github.com>
2022-05-16 16:20:34 +0300

    testing quality gate feature

--- TAF_IO/step_definitions/page_switcher.js
+++ TAF_IO/step_definitions/page_switcher.js
@@ -3,23 +3,23 @@ const pages = require('../page_objects/Pages');
 class PageSwitcher {
     constructor(){
         this.currentPageState = {};
-    }
+    };
 
     setState(pageName){
         this.state = pages[pageName];
-    }
+    };
 
     getState(){
         return this.state;
-    }
+    };
 
     getUrl() {
         return this.state.url;
-    }
+    };
 
     getElement(elementName){
         return this.state[elementName];
-    }
+    };
 }
 
 module.exports = new PageSwitcher (); 
